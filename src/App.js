07cf72d85a607bd27d759d2de377b358ea2c3109
Namnika <janbandhunamnika@gmail.com>
2025-06-09 10:40:59 +0530

    completed simple tasks of react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [sec, setSec] = useState(0)
+
+  const [show, setShow] = useState(true); // show/hide text
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSec((s) => s + 1)
+    }, 1000)
+
+    return () => clearInterval(timer); // cleanup timer 
+
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Task 1: Build a Counter App */}
+      <h2>Task 1: Build a Counter App</h2>
+
+      <h4>Counter: {count}</h4>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => setCount(0)}>reset</button>
+
+      {/* Task 2: Build a Timer App */}
+      <h2>Task 2: Build a Timer App</h2>
+
+      <h3>Timer: {sec} sec</h3>
+
+      {/* Task 3: Show/Hide Text */}
+      <h2>Task 3: Show/Hide Text</h2>
+
+      {show && <h3>this text can be hide or show</h3>}
+
+      <button onClick={() => setShow(!show)}>show/hide</button>
+
     </div>
   );
 }
