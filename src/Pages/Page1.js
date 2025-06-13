570e5d9b2007cac5cd53a5d02e4deb120fd4cb17
Namnika <janbandhunamnika@gmail.com>
2025-06-13 13:04:02 +0530

    removed styles

--- src/Pages/Page1.js
+++ src/Pages/Page1.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Page1 = ({ setCount, count }) => {
 
     return (
-        <div style={{ backgroundColor: "green", height: "50vh" }}>
+        <div >
             <button className='bg-[#f8fafc] border-solid border-2 border-sky-500 rounded-lg py-1.5 px-3 font-medium shadow-lg text-sky-500 ' onClick={() => setCount(count + 1)}>increment</button>
 
         </div>
